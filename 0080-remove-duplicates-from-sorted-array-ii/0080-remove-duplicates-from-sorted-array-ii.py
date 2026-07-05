802adeefc40e4a76d6d753e8c3441736bfd4571e
Chinta Rithvik <chintarithvik@gmail.com>
2026-07-05 23:52:39 +0530

    Time: 82 ms (82.79%), Space: 21.9 MB (82.04%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,13 +1,16 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        # i is our "slow pointer" that tracks where to place the next valid number
-        i = 0
+        # 1. Handle edge cases immediately
+        if len(nums) <= 2:
+            return len(nums)
         
-        for n in nums:
-            # We automatically accept the first two elements (i < 2)
-            # For subsequent elements, we check if it's strictly greater than the element two indices behind
-            if i < 2 or n > nums[i - 2]:
-                nums[i] = n
+        # 2. Skip the first two elements entirely
+        i = 2
+        
+        # 3. Iterate using range to avoid creating a new list slice in memory
+        for j in range(2, len(nums)):
+            if nums[j] > nums[i - 2]:
+                nums[i] = nums[j]
                 i += 1
                 
         return i
